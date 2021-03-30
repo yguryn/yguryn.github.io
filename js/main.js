0d736a356a464e5b7ff7078629bda87f77d9974a
yguryn <guryny@gmail.com>
2021-03-30 04:20:46 +0300

    fourth

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function (){
+    const burgerItem = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    burgerItem.addEventListener('click',() => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.addEventListener('click',() => {
+        menu.classList.remove('header__nav_active');
+    });
+}());
