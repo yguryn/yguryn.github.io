2baccbee569edb04225648a6e3e3b8e0fa9b1db8
yguryn <guryny@gmail.com>
2021-03-30 15:37:37 +0300

    final

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@
     });
 }());
 
+$(function (){
+  $('.slider').slick({
+      arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+  })
+})
+
